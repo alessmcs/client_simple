422ee3adfe26769d51c3dec06954a06a15d4822a
alessmcs <alessandramancas23@gmail.com>
2023-04-18 14:48:28 -0400

    added ClientView & clientController

--- src/mvc/ClientController.java
+++ src/mvc/ClientController.java
@@ -1,5 +1,11 @@
 package mvc;
 
+/**
+ * Cette classe lie le modèle avec la vue.
+ */
 public class ClientController {
+    private ClientModel modele;
+    private ClientView vue;
+
 
 }
---
+++ src/mvc/ClientView.java
@@ -0,0 +1,129 @@
+package mvc;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+
+    public class ClientView extends Application {
+
+        private TableView table = new TableView();
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage stage) {
+
+            Color bgColor = Color.BEIGE;
+
+            Scene scene = new Scene(new Group(), bgColor);
+            stage.setTitle("Inscription UdeM");
+            stage.setWidth(650);
+            stage.setHeight(500);
+
+            Label listeCours = new Label("Liste des Cours");
+            listeCours.setFont(new Font("Arial", 20));
+
+            table.setEditable(true);
+
+            TableColumn codeCol = new TableColumn("Code");
+            TableColumn classCol = new TableColumn("Cours");
+
+            table.getColumns().addAll(classCol, codeCol);
+            table.setPrefSize(250, 320);
+            ComboBox<String> comboBox = new ComboBox<>();
+            comboBox.getItems().addAll("Hiver", "Automne", "Été");
+            comboBox.setValue("Hiver");
+
+            Button chargerButton = new Button("charger");
+
+
+            Separator vertical = new Separator();
+            vertical.setPrefHeight(10);
+            vertical.setStyle("-fx-background-color: white;");
+            vertical.setOrientation(Orientation.VERTICAL);
+            vertical.setValignment(VPos.CENTER);
+
+            Separator horizontal = new Separator();
+            horizontal.setMaxWidth(250);
+            vertical.setPrefHeight(10);
+            horizontal.setStyle("-fx-background-color: white;");
+            horizontal.setOrientation(Orientation.HORIZONTAL);
+
+
+            Line line = new Line();
+            line.setStartX(0.0f); line.setStartY(0.0f); line.setEndX(100.0f); line.setEndY (100.0f);
+
+            final VBox vbox = new VBox();
+            vbox.setAlignment(Pos.CENTER);
+            vbox.setSpacing(5);
+            vbox.setPadding(new Insets(10, 10, 20, 20));
+
+            HBox hbox = new HBox();
+            hbox.setAlignment(Pos.CENTER);
+            hbox.setSpacing(10);
+            hbox.setPadding(new Insets(10));
+            hbox.getChildren().addAll(comboBox, chargerButton);
+            vbox.getChildren().addAll(listeCours, table, horizontal, hbox);
+
+            Label formulaireIns = new Label("Formulaire d'inscription");
+            formulaireIns.setFont(new Font("Arial", 21));
+
+            // Remplacez le HBox formHBox par un GridPane
+            GridPane formGrid = new GridPane();
+            formGrid.setAlignment(Pos.CENTER);
+            formGrid.setHgap(10);
+            formGrid.setVgap(10);
+            formGrid.setPadding(new Insets(20));
+
+            Label nomLabel = new Label("Nom");
+            TextField nomTextField = new TextField();
+            formGrid.addRow(0, nomLabel, nomTextField);
+
+            Label prenomLabel = new Label("Prénom");
+            TextField prenomTextField = new TextField();
+            formGrid.addRow(1, prenomLabel, prenomTextField);
+
+            Label emailLabel = new Label("Email");
+            TextField emailTextField = new TextField();
+            formGrid.addRow(2, emailLabel, emailTextField);
+
+            Label matriculeLabel = new Label("Matricule");
+            TextField matriculeField = new TextField();
+            formGrid.addRow(3, matriculeLabel, matriculeField);
+
+            Button inscrireButton = new Button("envoyer");
+            formGrid.addRow(5, inscrireButton);
+
+            VBox formVBox = new VBox();
+            formVBox.setAlignment(Pos.CENTER);
+            formVBox.setSpacing(5);
+            formVBox.setPadding(new Insets(10, 20, 20, 10));
+            formVBox.getChildren().addAll(formulaireIns, formGrid);
+
+            HBox mainHBox = new HBox();
+            mainHBox.setAlignment(Pos.CENTER);
+            mainHBox.setSpacing(10);
+            mainHBox.setPadding(new Insets(10));
+            mainHBox.getChildren().addAll(vbox, vertical, formVBox);
+
+            ((Group) scene.getRoot()).getChildren().addAll(mainHBox);
+
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
